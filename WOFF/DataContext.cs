2984721e030e737e8f8c5217a77b9a1a1ae51bea
turtle-insect <turtle.insect@gmail.com>
2018-11-12 10:05:44 +0900

    first commit

---
+++ WOFF/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOFF
+{
+	class DataContext
+	{
+		public DataContext()
+		{
+
+		}
+
+		public uint Money
+		{
+			get { return SaveData.Instance().ReadNumber(0x3B760, 4); }
+			set { Util.WriteNumber(0x3B760, 4, value, 0, 9999999); }
+		}
+	}
+}
